3e58a314755e4c5b6e07859dcccedc877f1b778a
Utkalika9 Nayak <utkalika.na@gmail.com>
2024-09-17 09:51:47 +0000

    Added create and getAllBooks

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const bookRoutes = require('./routes/bookRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ app.get("/", (req, res) => {
     res.send("HomePage Of The App");
 });
 
+app.use('/api',bookRoutes); // Use book routes with prefix '/api'
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
